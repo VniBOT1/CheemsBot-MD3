814e950aa4e4935200fe2aeb7ed0011e4fedcc00
VniBOT1 <110419673+VniBOT1@users.noreply.github.com>
2022-08-02 08:19:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,24 +41,24 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['6285850539404'] //ur owner number
-global.ownername = "6285850539404"
-global.ytname = "YT:https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //ur yt chanel name
-global.socialm = "GitHub: Jarot" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['6283183321692'] //ur owner number
+global.ownername = "6283183321692"
+global.ytname = "GADAK" //ur yt chanel name
+global.socialm = "GitHub: [ Jarot ]" //ur github or insta name
+global.location = "Indonesia, Medan, tembong" //ur location
 
 //bot bombdy
-global.botname = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄" //ur bot name
-global.websitex = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ" //ur website
+global.botname = "Robot WhatsApp" //ur bot name
+global.websitex = "GADAK" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/eFNTvNt.mp4' } //gif and video menu
-global.packname = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄ Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄"
+global.packname = "Robot WhatsApp Stickers\n\n\n\n\n\n\n🦄🗿🤧 "
+global.author = "Robot WhatsApp"
 global.themeemoji = "🐶"
-global.reactmoji = "😘"
-global.ownertag = ['6285850539404'] //ur owner tag
-global.ownernummenu = ['6285850539404'] //ur owner number in menu and all
-global.watermark = "𝐑𝐢𝐬𝐦𝐚-ᴍᴅ᭄." //ur watermark
-global.botscript = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.reactmoji = "🗿"
+global.ownertag = ['6283183321692'] //ur owner tag
+global.ownernummenu = ['6283183321692'] //ur owner number in menu and all
+global.watermark = "Robot WhatsApp." //ur watermark
+global.botscript = "CARI DI YT PAOK"
 global.linkz1 = ""
 global.linkz2 = ""
 
